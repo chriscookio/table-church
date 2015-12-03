aa0554c06319c8b7c5fc5cbefb17f3aa5176e8da
Chris Cook <cbrentcook@gmail.com>
2015-12-02 23:19:16 -0500

    added recent post thumbnails

--- front-page.php
+++ front-page.php
@@ -17,13 +17,8 @@ get_header(); ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php get_template_part( 'template-parts/content', 'page' ); ?>
-
-				<?php
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
-				?>
+                                
+                                <?php echo do_shortcode('[lptw_recentposts layout="grid-medium" post_type="post" link_target="self" fluid_images="true" min_height="0" space_hor="10" space_ver="10" columns="3" order="DESC" orderby="date" posts_per_page="10" post_offset="0" reverse_post_order="false" exclude_current_post="false" color_scheme="dark" override_colors="false" background_color="#4CAF50" text_color="#ffffff" excerpt_show="false" excerpt_lenght="35" ignore_more_tag="false" read_more_show="false" read_more_inline="false" read_more_content="Read more →" show_date_before_title="false" show_date="false" show_time="false" show_time_before="false" show_subtitle="false" date_format="d.m.Y" time_format="H:i" no_thumbnails="show"]'); ?>
 
 			<?php endwhile; // End of the loop. ?>
 
