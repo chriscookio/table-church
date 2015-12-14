b8ef57a522bf784babd4aa5d81c06043e5b48bca
Chris Cook <cbrentcook@gmail.com>
2015-12-13 21:10:12 -0500

    played with formatting on thumbnails

--- front-page.php
+++ front-page.php
@@ -11,19 +11,100 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main" role="main">
 
-			<?php while ( have_posts() ) : the_post(); ?>
+            <div class="s1" class="page-content">
 
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
-                                
-                                <?php echo do_shortcode('[lptw_recentposts layout="grid-medium" post_type="post" link_target="self" fluid_images="true" min_height="0" space_hor="10" space_ver="10" columns="3" order="DESC" orderby="date" posts_per_page="10" post_offset="0" reverse_post_order="false" exclude_current_post="false" color_scheme="dark" override_colors="false" background_color="#4CAF50" text_color="#ffffff" excerpt_show="false" excerpt_lenght="35" ignore_more_tag="false" read_more_show="false" read_more_inline="false" read_more_content="Read more →" show_date_before_title="false" show_date="false" show_time="false" show_time_before="false" show_subtitle="false" date_format="d.m.Y" time_format="H:i" no_thumbnails="show"]'); ?>
+                <?php
+                    if ( have_posts() ) {
+                        while ( have_posts() ) {
+                            the_post(); ?>
+                            <h2><?php the_title(); ?></h2>
+                            <?php the_content(); ?>
+                            <?php }
+                    }
+                ?>
+                <div class="container">
+                    <?php $postslist = get_posts('numberposts=4&order=DESC&orderby=date');
+                    foreach ($postslist as $post) :
+                        setup_postdata($post); ?>   
+                    <div class=col-md-3 class="sermonthumb" style="background-image: url('<?php echo wp_get_attachment_image_src(get_post_thumbnail_id() ) [0]; ?>'); background-repeat: no-repeat;">
+                    <?php echo the_date(); ?>
+                    </div>
+                    <?php endforeach;?>
+                </div>
+            </div>
+            
+            <div class="s2">
 
-			<?php endwhile; // End of the loop. ?>
+                <?php
+                    if ( have_posts() ) {
+                        while ( have_posts() ) {
+                            the_post(); ?>
+                            <h2><?php the_title(); ?></h2>
+                            <?php the_content(); ?>
+                            <?php }
+                    }
+                ?>
+            </div>
+            <div class="s3">
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+                <?php
+                    if ( have_posts() ) {
+                        while ( have_posts() ) {
+                            the_post(); ?>
+                            <h2><?php the_title(); ?></h2>
+                            <?php the_content(); ?>
+                            <?php }
+                    }
+                ?>
+            </div>
+            <div class="s4">
+
+                <?php
+                    if ( have_posts() ) {
+                        while ( have_posts() ) {
+                            the_post(); ?>
+                            <h2><?php the_title(); ?></h2>
+                            <?php the_content(); ?>
+                            <?php }
+                    }
+                ?>
+            </div>
+            <div class="s5">
+
+                <?php
+                    if ( have_posts() ) {
+                        while ( have_posts() ) {
+                            the_post(); ?>
+                            <h2><?php the_title(); ?></h2>
+                            <?php the_content(); ?>
+                            <?php }
+                    }
+                ?>
+            </div>
+            <div class="s6">
+
+                <?php
+                    if ( have_posts() ) {
+                        while ( have_posts() ) {
+                            the_post(); ?>
+                            <h2><?php the_title(); ?></h2>
+                            <?php the_content(); ?>
+                            <?php }
+                    }
+                ?>
+            </div>
+
+                <?php while ( have_posts() ) : the_post(); ?>
+
+                    <?php // get_template_part( 'template-parts/content', 'page' ); ?>
+
+                <?php endwhile; // End of the loop. ?>
+
+        </main><!-- #main -->
+    </div><!-- #primary -->
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
