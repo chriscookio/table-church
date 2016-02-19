2412e936e7e0a473c6abcb5c94414d1f19486984
Chris Cook <cbrentcook@gmail.com>
2016-02-18 23:16:00 -0500

    working on carousel.

--- include-hstreet.php
+++ include-hstreet.php
@@ -59,89 +59,73 @@
 <!--        LEADERSHIP      -->
 
 <div class="section" id='loc_leadership'>
-    <?php 
-    $leaderList = get_posts( [ post_type => 'leader' ] );
-    foreach ($leaderList as $leader) :
-        setup_postdata($leader); 
-        echo $the_title;
-    endforeach;
-    echo the_permalink() . "<br>";
-    ?>
-    Kevin Lum!!<br>
-    
-            <div class="row">
-            <?php 
-            $args = [
-                'post_type'        => 'leader',
-            ];
-            $postslist = get_posts($args);
-            foreach ($postslist as $post) :
-                setup_postdata($post); ?>   
-                <div class="col-md-3" style="sermon_thumb" >
-                    <?php
-                    $attrs = array(
-                    ); ?>
-                    <?php 
-                    the_post_thumbnail(array(200,200), $attrs );
-                    the_title();
-                    the_content();
-                    ?></a>
-                </div>
-            <?php endforeach;?>
-        </div>
-    
-
     <div id="leadershipCarousel" class="carousel slide" data-ride="carousel"> 
-    <div class="container">
-        <row>
-            <H1>Leadership</H1>
-            
-            <!-- Left and right controls -->
+        <div class="container">
+            <row>
+                <H1 class="col-md-10">Leadership</H1>
 
-            <a class="left carousel-control" href="#leadershipCarousel" role="button" data-slide="prev">
-                <img src="wp-content/themes/table-theme/img/arrow-left.gif" class='arrow' aria-hidden="false">
-            </a>
-            <a class="right carousel-control" href="#leadershipCarousel" role="button" data-slide="next">
-                <img src="wp-content/themes/table-theme/img/arrow-right.gif" class='arrow' aria-hidden="false">
-            </a>
-        </row>
-        <div class="row">
-            <div class="col-md-8 col-md-offset-4">
-                <div style="background-color: #F5FE03; height:10px;"></div>
+                <!-- Left and right controls -->
+
+                    <a class="left carousel-control col-md-1" href="#leadershipCarousel" role="button" data-slide="prev">
+                    <img src="wp-content/themes/table-theme/img/arrow-left.gif" class='arrow' aria-hidden="false">
+                    </a>
+                <a class="right carousel-control col-md-1" href="#leadershipCarousel" role="button" data-slide="next">
+                    <img src="wp-content/themes/table-theme/img/arrow-right.gif" class='arrow' aria-hidden="false">
+                </a>
+            </row>
+            <div class="row">
+                <div class="col-md-8 col-md-offset-4">
+                    <div style="background-color: #F5FE03; height:10px;"></div>
+                </div>
             </div>
         </div>
-    </div>
 
-      <!-- Indicators -->
+        
+      <!-- Indicators not needed -->
 
-      <ol class="carousel-indicators">
-        <li data-target="#leadershipCarousel" data-slide-to="0" class="active"></li>
-        <li data-target="#leadershipCarousel" data-slide-to="1"></li>
-      </ol>
+<!--        <div class="container">
+          <row>
+            <ol class="carousel-indicators col-md-offset-8 col-md-4">
+              <li data-target="#leadershipCarousel" data-slide-to="0" class="active"></li>
+              <li data-target="#leadershipCarousel" data-slide-to="1"></li>
+            </ol> 
+          </row>
+        </div>-->
 
-      <!-- Wrapper for slides -->
-      <div class="carousel-inner" role="listbox">
-        <div class="item active">
-          <img src="wp-content/themes/table-theme/img/logo.jpg" alt="c1" class='col-md-4'>
-          <p class='col-md-4'> <?php echo $lorem; ?> </p>
+        <!-- Wrapper for slides -->
+        <div class="container">
+            <div class="row">
+                <div class="carousel-inner col-md-offset-4 col-md-8" role="listbox">        
+                    <?php
+                    $i = 0;
+                    $args = [ 'post_type'        => 'leader', ];
+                    $postslist = get_posts($args);
+
+                    function leader_first() {
+                        global $i;
+                        if ($i == 0) : 
+                            echo "active"; 
+                        endif;
+                    }
+                    foreach ($postslist as $post) :
+                    setup_postdata($post);
+                    ?>
+                    <div class="item <?php leader_first(); ?>">
+
+                        <?php
+                        $attrs = array(); 
+                        the_post_thumbnail('thumbnail', $attrs );
+                        the_title();
+                        the_content();
+                        $i++;
+                        ?>
+
+                    </div>
+
+                <?php endforeach; ?>
+                </div>
+            </div>
         </div>
-
-        <div class="item">
-          <img src="wp-content/themes/table-theme/img/abe.jpg" alt="c2" class='col-md-4'>
-          <p class='col-md-4'> <?php echo $lorem; ?> </p>          
-        </div>
-
-      </div>
-
-      <!-- Left and right controls -->
-<!--      <a class="left carousel-control" href="#leadershipCarousel" role="button" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a class="right carousel-control" href="#leadershipCarousel" role="button" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-      </a>-->
     </div>
 </div>
 
