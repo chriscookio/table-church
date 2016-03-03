88920bade6d3887dae2d3dfe9f87861b029eb035
Chris Cook <cbrentcook@gmail.com>
2016-03-02 22:51:02 -0500

    sermon archive styles, text styles, general css fixes, sermon page styles

--- include-main.php
+++ include-main.php
@@ -162,11 +162,3 @@
         </div>
     </div>
 </div>
-
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -10,17 +10,17 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-    <header class="entry-header">
-            <?php 
-            the_title( '<h1 class="entry-title">', '</h1>' ); 
-            the_date("l, F j, Y",'<h2>','</h2>');
-            ?>
-
-            <!--<div class="entry-meta">-->
-                    <?php // table_theme_posted_on();  //// do not want ////  ?>
-            <!--</div> .entry-meta -->
-    </header><!-- .entry-header -->
-        <div class="container">
+    <div class="container">
+        <div class='row'>
+            <header class="entry-header">
+                <?php 
+                the_title( '<h1 class="entry-title">', '</h1>' ); 
+                echo '</row><row>';
+                the_date("l, F j, Y",'<h2>','</h2>');
+                ?>
+            </header><!-- .entry-header -->
+        </div>
+        <div class="entry-header">
             <div class="row">
                 <div class="col-md-offset-2 col-md-8 content-thumbnail"> 
                     <?php the_post_thumbnail(); ?> 
@@ -31,8 +31,6 @@
                         <?php the_content(); ?>
                 </div>
             </div>
-        </div>
-
     <?php
     wp_link_pages( array(
             'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'table-theme' ),
@@ -40,7 +38,9 @@
     ) );
     ?>
 
-    </div><!-- .entry-content -->
+        </div><!-- .entry-content -->
+    </div><!-- .container -->
+
     <footer class="entry-footer">
             <?php // table_theme_entry_footer(); //// just the "edit" link...no need //// ?>
     </footer><!-- .entry-footer -->
